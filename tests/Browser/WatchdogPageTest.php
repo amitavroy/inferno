816f07fa20ab7c0a9ff383cde209997f58fb6629
Amitav Roy <reachme@amitavroy.com>
2017-02-10 12:31:35 +0530

    Now I am able to check the latest entry

--- tests/Browser/WatchdogPageTest.php
+++ tests/Browser/WatchdogPageTest.php
@@ -8,6 +8,13 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class WatchdogPageTest extends DuskTestCase
 {
+    private $pageUrl;
+
+    function __construct()
+    {
+        $this->pageUrl = '/config/activities';
+    }
+
     /**
      * A Dusk test example.
      *
@@ -17,20 +24,34 @@ class WatchdogPageTest extends DuskTestCase
     {
         $this->browse(function ($browser) {
             $browser->loginAs(User::find(1))
-                    ->visit('/config/activities')
-                    ->assertSee('My activities');
+                ->visit($this->pageUrl)
+                ->assertSee('My activities');
         });
     }
 
+    /**
+     * Test on search the text value continues to remain in the text box
+     *
+     * @throws \Exception
+     * @throws \Throwable
+     */
     public function testSearchTextRemains()
     {
-        $this->browse(function ($browser) {
-            $browser->visit('/config/activities')
-                ->type('search_text', 'Hello world')
+        $searchText = 'Hello world';
+        $this->browse(function ($browser) use ($searchText) {
+            $browser->visit($this->pageUrl)
+                ->type('search_text', $searchText)
                 ->click('.btn-success')
-                ->assertPathIs('/config/activities')
+                ->assertPathIs($this->pageUrl)
                 ->assertSee('My activities')
-                ->assertInputValue('search_text', 'Hello world');
+                ->assertInputValue('search_text', $searchText);
+        });
+    }
+
+    public function testFirstRowOfTableIsLoginEntry()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit($this->pageUrl);
         });
     }
 }

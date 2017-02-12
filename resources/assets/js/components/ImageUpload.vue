90520805297dca2f270b72d81dd2da102afb7ba4
Amitav Roy <reachme@amitavroy.com>
2017-02-12 09:04:01 +0530

    Before merging master

---
+++ resources/assets/js/components/ImageUpload.vue
@@ -0,0 +1,41 @@
+<script>
+  import Croppie from 'croppie'
+  import $ from 'jquery'
+  export default {
+    props: ['imgUrl', 'alt'],
+    mounted () {
+      let el = document.getElementById('croppie')
+      let opts = {
+        enableExif: true,
+        viewport: {
+          width: 100,
+          height: 100,
+          type: 'circle'
+        }
+      }
+      this.croppie = new Croppie(el, {
+        viewport: { width: 200, height: 200 },
+        boundary: { width: 220, height: 220 },
+        showZoomer: true,
+        enableOrientation: true
+      })
+      this.croppie.bind({
+        url: this.imgUrl
+      })
+    },
+    data () {
+      return {
+        croppie: null
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="Image-upload-wrapper Image-upload">
+    <p>Image</p>
+    <!-- <img v-bind:src="imgUrl" v-bind:alt="alt" id="croppie"> -->
+    <div id="croppie"></div>
+    <!-- <img v-src="imgUrl" alt="{{ alt }}" id="croppie"> -->
+  </div>
+</template>
